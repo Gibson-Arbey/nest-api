48d50d102de1ed5b4bde6d646424c39a32681603
Gibson-Arbey <gibsonarbey@gmail.com>
2024-12-22 13:01:14 -0500

    first commit

---
+++ src/products/entities/product.entity.ts
@@ -0,0 +1,46 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ schema: 'ecommerce' })
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('text', {
+    unique: true,
+  })
+  titlw: string;
+
+  @Column('numeric', {
+    default: 0,
+  })
+  price: number;
+
+  @Column({
+    type: 'text',
+    nullable: true,
+  })
+  description: string;
+
+  @Column({
+    type: 'text',
+    unique: true,
+  })
+  slug: string;
+
+  @Column({
+    type: 'int',
+    default: 0,
+  })
+  stock: number;
+
+  @Column({
+    type: 'text',
+    array: true,
+  })
+  sizes: string[];
+
+  @Column({
+    type: 'text',
+  })
+  gender: string;
+}
